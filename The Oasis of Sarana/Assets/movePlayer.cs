9e5ec1ef742fac6aa12d999396f1c11e4cb04f19
Andre Cardoso <amc.acardoso@gmail.com>
2016-04-21 18:04:21 -0400

    2d and 3d test

---
+++ The Oasis of Sarana/Assets/movePlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+	private Vector3 hSpeed;
+	private Vector3 vSpeed;
+	private Vector3 zeroSpeed;
+	public float velocity;
+	private float xscale;
+
+	// Use this for initialization
+	void Start () {
+
+		xscale = transform.localScale.x;
+	
+		hSpeed = new Vector3 (velocity, 0, 0);
+		vSpeed = new Vector3 (0, velocity, 0);
+		zeroSpeed = new Vector3 (0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.localScale = new Vector3 (xscale, transform.localScale.y, transform.localScale.z);
+			transform.Translate (Vector3.right * velocity * Time.deltaTime);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			transform.localScale = new Vector3 (-xscale, transform.localScale.y, transform.localScale.z);
+			transform.Translate (-Vector3.right * velocity * Time.deltaTime);
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Translate (Vector3.up * velocity * Time.deltaTime);
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			transform.Translate (-Vector3.up * velocity * Time.deltaTime);
+		}
+	
+	}
+}
---
+++ The Oasis of Sarana/Assets/rotateAround.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateAround : MonoBehaviour {
+
+	public float speed;
+	public float jumpSpeed;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.Rotate (-Vector3.up * speed * Time.deltaTime);
+		}
+
+		if (Input.GetAxis ("Horizontal") < 0) {
+			transform.Rotate (Vector3.up * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && gameObject.CompareTag("Player")) {
+			player.GetComponent<Rigidbody> ().AddForce (Vector3.up * jumpSpeed);
+		}
+	
+	}
+}
