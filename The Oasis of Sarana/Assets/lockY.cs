d67e366bf728f558991c4c4f6e472c7ee3002fa1
Andre Cardoso <amc.acardoso@gmail.com>
2016-04-27 14:06:28 -0400

    particle system test

---
+++ The Oasis of Sarana/Assets/lockY.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lockY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			Time.timeScale = 0.1f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			Time.timeScale = 1;
+		}
+
+		transform.position = new Vector3 (transform.position.x, 2.611089f, transform.position.z);
+	
+	}
+}
